24344bfa951d0ec2a88aa4ee0849cf92293e06ca
senbonsakura <overlord_cihat@yahoo.com>
2020-10-02 16:01:31 +0300

    Added create ingredient endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -48,3 +48,11 @@ class ModelTests(TestCase):
         """Test the tag string representation"""
         tag = models.Tag.objects.create(user=sample_user(), name='Test Tag')
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredients string representation"""
+        ingredient = models.Ingredient.objects.create(
+                user=sample_user(),
+                name='Flour'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 router = DefaultRouter()
 
 router.register('tags', views.TagViewSet)
+router.register('recipes', views.IngredientViewSet)
 
 app_name = 'recipe'
 
